2d3d0e220c09d927ddb628c2afc9dbff3139e109
617702 <617702@ELF6IT215744.ACDM.DS.SAIT.CA>
2017-09-19 11:54:01 -0600

    New Project Creation Created a new Java Application for HelloWorld

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -14,7 +14,9 @@ public class HelloWord {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
         // TODO code application logic here
     }
     
